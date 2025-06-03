34b1cb5ae9247883837eb0b5689a1c7e285d86d5
Massingue2025 <antoniocarlosmassingue20@gmail.com>
2025-06-03 22:00:02 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -2,7 +2,14 @@
   deps = [
     pkgs.python311
     pkgs.ffmpeg
-    pkgs.python311Packages.flask
-    pkgs.python311Packages.werkzeug
+    pkgs.git
   ];
+
+  env = {
+    PYTHON_LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  };
+
+  postInstall = ''
+    pip install flask pillow torch torchvision realesrgan
+  '';
 }
